a9ccd070df3b9f4b733f847c68db384f78befabf
Mohit Soni <ms3734@cornell.edu>
2024-08-05 17:01:39 -0700

    Connected MongoDB using Mongoose
    - Set the schema for the Master collection
    - Used forms to send data from the frontend to the backend
    - Added data to the Master collection in the backend

--- app.js
+++ app.js
@@ -23,11 +23,63 @@ app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+/////////////////////////////////////////
+/********* Setting up Database *********/
+/////////////////////////////////////////
+
+mongoose.connect('mongodb://localhost:27017/db')
+.then(() => (
+    console.log('MongoDB connected.')
+))
+.catch(() => (
+  console.log('ERROR: MongoDB could not connect.')
+));
+
+const MasterSchema = mongoose.Schema({
+  type: { 
+    type: String, 
+    required: true 
+  },
+  brand: { 
+    type: String, 
+    required: true 
+  },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  uom: { 
+    type: String, 
+    required: true
+  },
+  margin: { 
+    type: Number, 
+    required: true 
+  }
+});
+
+const Master = mongoose.model('master', MasterSchema);
+
 /////////////////////////////
 /********* Routing *********/
 /////////////////////////////
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.post('/addmaster', (req, res) => {
+  data = {
+    type: req.body.type,
+    brand: req.body.brand,
+    name: req.body.name,
+    uom: req.body.uom,
+    margin: req.body.margin
+  };
+
+  Master.insertMany([data]);
+  console.log(Master);
+  console.log('Added items.');
+  res.redirect('/');
 });
 
